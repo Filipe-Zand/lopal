6b6764b609c6a988c669d42910c4c5b107178b95
Filipe-Zand <zandonadi.filipe2010@gmail.com>
2026-03-04 16:50:48 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,9 +1,12 @@
 function calculadora(){
+    
     while(true){
         let resultado = 0.0;
         let operando1;
         let operando2;
         let operador;
+        let continua;
+        let operador_valido = true;
 
         operando1 = parseFloat (prompt("Digite o primeiro número: "));
         operando2 = parseFloat (prompt("Digite o segundo número: "));
@@ -16,14 +19,25 @@ function calculadora(){
         } else if ( operador == "*" ){
             resultado = operando1 * operando2;
         } else if ( operador == "/" ){
+            if (operando2 == 0){
+                alert("não é possivel dividir por zero");
+                operador_valido = false;
+                continua = prompt("Digite sim para continuar e não para encerrar.");
+                if( continua === "não" ){
+                    return
+                }
+            } else {
             resultado = operando1 / operando2;
+            }
         } else {
             alert("operador inválido!");
             return
-        } if (operador2 == 0)
-            alert("não é possivel dividir por zero")
-            return
-        {alert("Resultado: " + operando1 + " " + 
-           operador + " " + operando2 + " = " + resultado);}
+        } 
+        
+        if( operador != "/" || operando2 != 0){
+             alert("Resultado: " + operando1 + " " + 
+             operador + " " + operando2 + " = " + resultado);
+        }
+       
     }
-}    
+}
